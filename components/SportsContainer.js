01f1c7016b8d44178724073aa23217f10875330e
Frunza <frunza.andrei.daniel@gmail.com>
2021-05-01 06:37:49 +0300

    created the sportContainer component

---
+++ components/SportsContainer.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import colors from '../colors';
+import Loading from '../screens/utils/Loading';
+
+const SportsContainer = ({
+  iconSport,
+  iconRun,
+  timeRunning,
+  time,
+  title,
+  isLoading = false,
+  styleIconSport,
+}) => {
+  return (
+    <View style={styles.container}>
+      {iconSport && (
+        <Image source={iconSport} style={[styles.iconSport, styleIconSport]} />
+      )}
+      <View>
+        {title && <Text style={styles.text}>{title}</Text>}
+        <Text style={styles.text}>{`${timeRunning} | ${time} min`}</Text>
+      </View>
+      <View style={styles.containerIcon}>
+        <Loading isLoading={isLoading} />
+        {iconRun && !isLoading && (
+          <Image source={iconRun} style={styles.iconRun} />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default SportsContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: colors.backgroundTabBar,
+    marginHorizontal: 40,
+    marginVertical: 10,
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 10,
+    elevation: 6,
+  },
+  iconSport: {
+    width: 30,
+    height: 50,
+    marginVertical: 6,
+  },
+  iconRun: {
+    width: 24,
+    height: 24,
+  },
+  containerIcon: {
+    padding: 10,
+    backgroundColor: colors.background,
+    borderRadius: 25,
+    elevation: 4,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
